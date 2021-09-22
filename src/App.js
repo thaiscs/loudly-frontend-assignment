707fe77eb7a7856eca3209a28d82c641f48d3bb3
thaiscs <thais.advj@gmail.com>
2021-09-22 15:16:35 +0200

    implemented clean solution with react audio object + connected src files with play/pause btns + got started in a solution to pause previous song

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from "./logo.svg";
 import * as React from "react";
 import "./App.css";
 import { useEffect, useState } from "react";
 import MusicPlayer from "./components/MusicPlayer";
+import audioFile from "./audioSample.mp3";
 
 type Soundtracks = {
   list: Array<Object>,
 };
 
-// use this as Hook to pass data to the APP
+// use this component as a Hook to pass data down
 const App = () => {
   const [soundtracks, setSoundtracks] = useState([]); // default value
   // (soundtracks: Array<object>);
-  // I wanted Flow typing to check if soundtracks was returning an array of objects, but didn't find good solutions online on how to that properly, so given the short time that I have, I've decided to focus on implementing a MVP (song player)first.
+  // I wanted Flow typing to check if soundtracks was returning an array of objects, but didn't find good solutions online on how to do that properly, so given the short time that I have, I've decided to focus on implementing an MVP (song player)first.
+  const [musicSrc, setMusicSrc] = useState([]);
 
   useEffect(() => {
     fetch("https://api-stg.jam-community.com/song/trending").then(
@@ -20,9 +21,11 @@ const App = () => {
         response
           .json()
           .then((data) => {
-            // it would more practical if the response already contained the song src files
+            // it would more practical if the response file already contained the song sources
             setSoundtracks(data);
-
+            // so I've decided to create my own src list with a sample file
+            // this way I don't waste time searching manually for each of them
+            setMusicSrc(Array(data.length).fill(audioFile));
             console.log(data);
           })
           .catch((error) => console.log(error)); // with more time I could customize an error page
@@ -30,10 +33,10 @@ const App = () => {
     );
   }, []);
 
-  // would do a TEST to check if Music Player component is present
+  // could do a TEST to check if Music Player component is present and has appropriate props
   return (
     <div className="App">
-      <MusicPlayer soundtracks={soundtracks} />
+      <MusicPlayer soundtracks={soundtracks} musicSrc={musicSrc} />
     </div>
   );
 };
--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -2,28 +2,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import { faPauseCircle } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-const MusicPlayer = ({ soundtracks }) => {
+const MusicPlayer = ({ soundtracks, musicSrc }) => {
+  const music = musicSrc.map((src) => new Audio(src));
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentPlaying, setCurrentPlaying] = useState(null);
+
+  const pausePreviousSong = () => {
+    console.log("prev", music[currentPlaying]);
+    if (isPlaying) music[currentPlaying].pause();
+  };
+
   return (
     <ul className="music-list">
-      {soundtracks.map((song) => (
+      {soundtracks.map((song, key) => (
         <li key={song.id} className="soundtrack">
           <div className="thumbnail">
-            <a target="_blank" href={song.cover_image_path}>
-              <img src={song.cover_image_path} atl="cover song image" />
+            <a target="_blank" href={song.cover_image_path} rel="noreferrer">
+              <img
+                src={song.cover_image_path}
+                alt={"music " + song.name + " cover"}
+              />
             </a>
           </div>
-
           <div className="song-title">{song.name}</div>
-
-          <div className="play-button">
+          <div className="play-button" onClick={() => music[key].play()}>
             <FontAwesomeIcon icon={faPlayCircle} />
           </div>
-
-          <div className="pause-button">
+          <div className="pause-button" onClick={() => music[key].pause()}>
             <FontAwesomeIcon icon={faPauseCircle} />
           </div>
-
           <div className="like-button">
             <FontAwesomeIcon icon={faHeart} />
           </div>
